df38b981f0b0cc89f595815795a9e5e499137467
Miguel Colque <micky.cm@outlook.es>
2024-05-17 10:38:30 -0400

    Se modificó el archivo script.js para utilizar async y await

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const nasaAPI = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15"
 
+// async y await
+async function listaImagenes() {
+  try{
+    let fetchImagen = await fetch(nasaAPI)
+    let datosImagenes = await fetchImagen.json()
+    const card = document.querySelector("[data-ul]")
+
+    datosImagenes.forEach(element => {
+      const contenido = `
+      <li class="card">
+                <img class="card__image" src="${element.url}" alt="imagen">
+                <h3 class="card__title">${element.title}</h3>
+            </li>
+      `
+      card.innerHTML = card.innerHTML + contenido
+    });
+  }
+  catch(error) {
+    console.log(error)
+  }
+}
+
+
 // then y catch
-function listaImagenes() {
+/* function listaImagenes() {
   fetch(nasaAPI)
   .then(response => response.json())
   .then(datosImagenes => {
@@ -20,6 +43,6 @@ function listaImagenes() {
     })
   })
   .catch(error => console.log(error))
-}
+} */
 
 listaImagenes()
