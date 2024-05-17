87bd575ba249a733bafda95e7560fa3a1e4531e3
Miguel Colque <micky.cm@outlook.es>
2024-05-17 10:36:34 -0400

    Se creó el archivo script.js para implementar la API de la NASA utilizando then y catch

---
+++ script.js
@@ -0,0 +1,25 @@
+const nasaAPI = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15"
+
+// then y catch
+function listaImagenes() {
+  fetch(nasaAPI)
+  .then(response => response.json())
+  .then(datosImagenes => {
+    console.log(datosImagenes)
+
+    const card = document.querySelector("[data-ul]")
+
+    datosImagenes.forEach(element => {
+      const contenido = `
+      <li class="card">
+                <img class="card__image" src="${element.url}" alt="imagen">
+                <h3 class="card__title">${element.title}</h3>
+            </li>
+      `
+      card.innerHTML = card.innerHTML + contenido
+    })
+  })
+  .catch(error => console.log(error))
+}
+
+listaImagenes()
